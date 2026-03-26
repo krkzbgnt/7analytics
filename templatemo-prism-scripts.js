068b40f4ba1617bb7f9f36e96663960cc0b2fa1c
krkzbgnt <akbognot23-0214@cca.edu.ph>
2026-03-26 22:12:09 +0800

    Update templatemo-prism-scripts.js

--- templatemo-prism-scripts.js
+++ templatemo-prism-scripts.js
@@ -63,7 +63,7 @@ https://templatemo.com/tm-600-prism-flux
             {
                 id: 7,
                 title: 'Midterm Task 2',
-                description: 'Intelligent IoT ecosystem connecting millions of devices with edge computing capabilities.',
+                description: 'Data Preparation and Modeling using Power Query',
                 image: 'images/imgMterm2.png',
                 tech: ['MQTT', 'Edge AI', '5G'],
                 pdf: 'https://drive.google.com/drive/folders/1XApsIBS9NF6Sximhjob3KEevmt4sGu0R?usp=sharing'
